cdc8680186f50a6cada82f43c3060e6b0d24934c
kozulova <benedukvalia@gmail.com>
2021-03-18 17:59:27 +0200

    error fixed

--- src/App.js
+++ src/App.js
@@ -4,28 +4,42 @@ import './App.css';
 function App() {
   const [weatherData, setWeatherData] = useState({})
   const [location, setLocation] = useState('London')
+  const [error, setError] = useState('')
 
   useEffect(() => {
-
     getWeather('London');
-
   }, [])
+
+  useEffect(()=>{
+    setTimeout(()=>setError(''), 3000)
+  }, [error])
 
   const getWeather = async (local) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${local}&appid=${process.env.REACT_APP_APIKey}`;
     const response = await fetch(url);
-    const weatherDate = await response.json();
-    const weatherMain = weatherDate.main;
-    setWeatherData(weatherMain);
+      if(response.ok){
+        const weatherDate = await response.json();
+        const weatherMain = weatherDate.main;
+        setWeatherData(weatherMain);
+      }
+      else{
+        setError(`Weather wasn't found because of ${response.status}`)
+        setLocation('London')
+      }
+    
   }
 
 
   const findWeather = (event) => {
 
     event.preventDefault()
-    const locationInput = document.querySelector('.location');
-    setLocation(locationInput.value)
-    getWeather(locationInput.value)
+
+    const locationInput = document.querySelector('.location')
+    const locaionInputValue = locationInput.value
+
+    setLocation(locaionInputValue)
+    getWeather(locaionInputValue)
+
     locationInput.value = ''
 
   }
@@ -48,6 +62,8 @@ function App() {
       <p>
         {weatherData.humidity < 80 ? String.fromCharCode(9728) : String.fromCharCode(9730)}
       </p>
+
+      <p>{error}</p>
       <form onSubmit={findWeather}>
         <input type="text" className="location" />
         <input type="submit" value="Find weather" />
